a79cf12aeb05aa606d0740c298d566ee59253053
rohoehn <rohoehn123@gmail.com>
2025-05-26 21:12:46 +0200

    added brew installed packages to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,14 @@ end
 # use vi-keybindings
 set -g fish_key_bindings fish_vi_key_bindings
 
+# Add the homebrew bin and sbin directories to the PATH
+if test -d /opt/homebrew/bin
+    set -gx PATH /opt/homebrew/bin $PATH
+end
+
+if test -d /opt/homebrew/sbin
+    set -gx PATH /opt/homebrew/sbin $PATH
+end
 # activate miniconda if exists
 set CONDA_FISH_INIT_FILE "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
 
