76b8517f6cde27e5abc306e45bee1eb487b8e5d8
rohoehn <rohoehn123@gmail.com>
2025-05-26 21:02:57 +0200

    fixed opam ref in fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,12 +11,3 @@ set CONDA_FISH_INIT_FILE "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
 if test -e $CONDA_FISH_INIT_FILE
     source $CONDA_FISH_INIT_FILE
 end
-
-
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/Users/roman/.opam/opam-init/init.fish' && source '/Users/roman/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
