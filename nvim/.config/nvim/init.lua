e7d72f190c96afb50b25538c0a246633e0e129cb
rohoehn <rohoehn123@gmail.com>
2026-01-01 11:57:16 +0100

    set up neovim copilot and python lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -8,3 +8,9 @@ local settings_file = os.getenv("HOME") .. "/.vim/settings.vim"
 if vim.fn.filereadable(settings_file) == 1 then
   vim.cmd("source " .. settings_file)
 end
+
+-- If we arent running inside VSCode, we want to use Lazy.nvim as our Plugin-Manager
+if not vim.g.vscode then
+    require("config.lazy")
+end
+
