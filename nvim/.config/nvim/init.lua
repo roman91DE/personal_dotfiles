779db268c358c48be2605252965f30872ab51da8
rohoehn <rohoehn123@gmail.com>
2026-01-15 17:50:45 +0100

    added lazy vim distro and seperate vs_code neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,2 @@
--- ============================================================================
--- NEOVIM CONFIGURATION
--- ============================================================================
--- Sources shared settings for Vim and Neovim
-
--- Load shared settings from Vim configuration
-local settings_file = os.getenv("HOME") .. "/.vimrc"
-if vim.fn.filereadable(settings_file) == 1 then
-  vim.cmd("source " .. settings_file)
-end
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ vscode_nvim/.config/vscode_nvim/init.lua
@@ -0,0 +1,11 @@
+-- ============================================================================
+-- NEOVIM CONFIGURATION
+-- ============================================================================
+-- Sources shared settings for Vim and Neovim
+
+-- Load shared settings from Vim configuration
+local settings_file = os.getenv("HOME") .. "/.vimrc"
+if vim.fn.filereadable(settings_file) == 1 then
+  vim.cmd("source " .. settings_file)
+end
+
