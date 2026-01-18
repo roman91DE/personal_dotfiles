89bdf82daaf95d7cd8186380ae136aafe778107b
rohoehn <rohoehn123@gmail.com>
2026-01-18 11:28:43 +0100

    some key remaps

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- control + c to escape in insert mode
+vim.keymap.set({ "i", "v", "s" }, "<C-c>", "<Esc>", { noremap = true })
+-- _ behaves likes ^ in both normal and visual mode
+vim.keymap.set({ "n", "v" }, "_", "^", { noremap = true })
