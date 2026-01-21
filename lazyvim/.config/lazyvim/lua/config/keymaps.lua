0406a87150bd6239eea195303e8c12283dbd385a
rohoehn <rohoehn123@gmail.com>
2026-01-21 22:19:40 +0100

    auto git command

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -11,20 +11,22 @@ vim.keymap.set({ "n", "v" }, "_", "^", opts)
 -- terminal mode keymaps
 --  map <Esc> to exit terminal-mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
+-- Use Ctrl + C to use vim mode on line
+vim.keymap.set("t", "<C-c>", "<Esc>", opts)
 
 -- Window Navigation using Alt + h/j/k/l for all modes
 -- Terminal mode
-vim.api.nvim_set_keymap("t", "<A-h>", [[<C-\><C-N><C-w>h]], opts)
-vim.api.nvim_set_keymap("t", "<A-j>", [[<C-\><C-N><C-w>j]], opts)
-vim.api.nvim_set_keymap("t", "<A-k>", [[<C-\><C-N><C-w>k]], opts)
-vim.api.nvim_set_keymap("t", "<A-l>", [[<C-\><C-N><C-w>l]], opts)
+vim.api.nvim_set_keymap("t", "<C-h>", [[<C-\><C-N><C-w>h]], opts)
+vim.api.nvim_set_keymap("t", "<C-j>", [[<C-\><C-N><C-w>j]], opts)
+vim.api.nvim_set_keymap("t", "<C-k>", [[<C-\><C-N><C-w>k]], opts)
+vim.api.nvim_set_keymap("t", "<C-l>", [[<C-\><C-N><C-w>l]], opts)
 -- Insert mode
-vim.api.nvim_set_keymap("i", "<A-h>", [[<C-\><C-N><C-w>h]], opts)
-vim.api.nvim_set_keymap("i", "<A-j>", [[<C-\><C-N><C-w>j]], opts)
-vim.api.nvim_set_keymap("i", "<A-k>", [[<C-\><C-N><C-w>k]], opts)
-vim.api.nvim_set_keymap("i", "<A-l>", [[<C-\><C-N><C-w>l]], opts)
+vim.api.nvim_set_keymap("i", "<C-h>", [[<C-\><C-N><C-w>h]], opts)
+vim.api.nvim_set_keymap("i", "<C-j>", [[<C-\><C-N><C-w>j]], opts)
+vim.api.nvim_set_keymap("i", "<C-k>", [[<C-\><C-N><C-w>k]], opts)
+vim.api.nvim_set_keymap("i", "<C-l>", [[<C-\><C-N><C-w>l]], opts)
 -- Normal mode
-vim.api.nvim_set_keymap("n", "<A-h>", "<C-w>h", opts)
-vim.api.nvim_set_keymap("n", "<A-j>", "<C-w>j", opts)
-vim.api.nvim_set_keymap("n", "<A-k>", "<C-w>k", opts)
-vim.api.nvim_set_keymap("n", "<A-l>", "<C-w>l", opts)
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
